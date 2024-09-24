1a703f4aa2b6a13c17d275ea7f5500d7c4fe3f5b
Weslley Costa <weslleycosta.ti@gmail.com>
2024-09-24 13:01:26 -0300

    "Criação de novas telas criação de arquivo styles"

--- app/CategoryScreen.js
+++ app/CategoryScreen.js
@@ -1,62 +1,30 @@
-import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { router } from "expo-router";
+import { styles } from "./styles";
 
 export default CategoryScreen = () => {
-  const destinations = ['Teresina', 'Timon', 'Parnaiba', 'Campo Maior'];
+  const destinations = ['Teresina', 'Timon', 'Parnaiba', 'CampoMaior'];
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Cidades</Text>
       </View>
-
+      <View style={styles.main}> 
       <Text style={styles.title}>Escolha seu Destino!! </Text>
 
-      { destinations.map(destinations =>(
+      {/* { destinations.map(destinations =>(
       <TouchableOpacity
         style={styles.touchableOpacity}
         onPress={() => {router.push("/HomeScreen")}}>
         <Text style={styles.buttonText}>{destinations}</Text>
       </TouchableOpacity>
-      )) }
+      )) } */}
+            {destinations.map(places => (
+        <TouchableOpacity style={styles.touchableOpacity} onPress={() => {router.push({pathname : "/DetailsScreen" , params : {destination : places}})}}>
+            <Text style={styles.buttonText}>{places}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>      
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  header: {
-    width: "100%",
-    padding: 20,
-    backgroundColor: "#2C3E50",
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: "5%",
-    marginTop: "15%",
-    color: "#2C3E50",
-  },
-  touchableOpacity: {
-    backgroundColor: "#7F8C8D",
-    padding: 20,
-    alignItems: "center",
-    borderRadius: 20,
-    marginTop: 40,
-    width: "50%",
-  },
-  buttonText: {
-    color: "#ffffff",
-    fontSize: 16,
-  },
-});
--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { router } from "expo-router";
+import { styles } from "./styles";
 
 export default HomeScreen = () => {
   return (
@@ -8,7 +9,7 @@ export default HomeScreen = () => {
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Home</Text>
       </View>
-
+      <View style={styles.main}> 
       <Text style={styles.title}>Bem Vindo!! </Text>
       <TouchableOpacity
         style={styles.touchableOpacity}
@@ -18,44 +19,7 @@ export default HomeScreen = () => {
       >
         <Text style={styles.buttonText}>Veja as Opções</Text>
       </TouchableOpacity>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  header: {
-    width: "100%",
-    padding: 20,
-    backgroundColor: "#2C3E50",
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: "5%",
-    marginTop: "50%",
-    color: "#2C3E50",
-  },
-  touchableOpacity: {
-    backgroundColor: "#7F8C8D",
-    padding: 20,
-    alignItems: "center",
-    borderRadius: 20,
-    marginTop: 40,
-    width: "90%",
-  },
-  buttonText: {
-    color: "#ffffff",
-    fontSize: 16,
-  },
-});
